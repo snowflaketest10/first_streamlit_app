80d1dfd28eadc2c7bf0a35837ffe41f365302dfd
snowflaketest10 <110574494+snowflaketest10@users.noreply.github.com>
2022-08-04 17:59:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,3 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response=request.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
