d496664449cab73df7a2591902f4e6d82811ffbd
snowflaketest10 <110574494+snowflaketest10@users.noreply.github.com>
2022-08-05 08:48:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,3 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#New Section to write Fruitvice API Response
-streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
