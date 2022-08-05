c67b2f371e5aaa900a03139eba51d56e1beea68c
snowflaketest10 <110574494+snowflaketest10@users.noreply.github.com>
2022-08-05 09:37:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,17 +33,26 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruitjuice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
+Try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice: 
+      streamlit.error("Please selet a fruit to get information.")
+      #streamlit.write('The user entered ', fruit_choice)
+      else
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
 
 
 # Take the jason version of response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output in the screen as table 
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
